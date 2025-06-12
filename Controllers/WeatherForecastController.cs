70cf25767e6c695fec0edf1ce47334ae9323e12a
Sriram <sriram.openwave@gmail.com>
2025-06-12 22:58:09 +0530

    New Array and New methods introduced

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 
 namespace WebApplication1.Controllers;
 
@@ -10,6 +13,14 @@ public class WeatherForecastController : ControllerBase
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Very Hot"
     };
+
+
+    private static readonly List<int> items = new()  
+    {
+        1, 2, 3, 4, 5, 6,
+    };
+  
+     
 
     private readonly ILogger<WeatherForecastController> _logger;
 
@@ -28,5 +39,18 @@ public class WeatherForecastController : ControllerBase
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
+    }
+
+
+    [HttpGet]
+    public  async Task<IActionResult> GetTempAsync()
+    {
+        return await Task.FromResult(Ok("Success"));
+    }
+
+    [HttpGet]
+    public IActionResult GetTempSync()
+    {
+        return Ok("Success");
     }
 }
